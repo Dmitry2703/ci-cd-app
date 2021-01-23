8a56265762144404947908e465c58700dae3576c
dmeshcheryakov <Dmitriy.Meshcheryakov@noveogroup.com>
2021-01-23 11:47:05 +0600

    Change main page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders main page text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Test project for CI\/CD learning/i);
+  expect(textElement).toBeInTheDocument();
 });
