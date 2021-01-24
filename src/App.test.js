9b36afe048e5fc7af27a04e6d061362f24b9c95d
dmeshcheryakov <Dmitriy.Meshcheryakov@noveogroup.com>
2021-01-24 11:40:53 +0600

    Deploy

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders main page text', () => {
-  render(<App />);
-  const textElement = screen.getByText(/Test project for CI\/CD learning/i);
-  expect(textElement).toBeInTheDocument();
-});
